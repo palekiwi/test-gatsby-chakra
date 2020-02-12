b2f918399a1d44dd1ac914563fd73d06a199f0c5
Pawel Lisewski <pawel.lisewski@gmail.com>
2020-02-12 19:00:36 +0800

    install chakra-ui

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
+import { theme, ThemeProvider, CSSReset } from "@chakra-ui/core";
+//import { customTheme } from "../theme";
 
 interface Props {
   children?: React.ReactNode;
@@ -17,13 +19,14 @@ const Layout: React.FC<Props> = ({ children }) => {
   `);
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CSSReset />
       <div>{data.site.siteMetadata.title}</div>
       <div>
         <main>{children}</main>
         <footer>footer</footer>
       </div>
-    </>
+    </ThemeProvider>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { Button } from "@chakra-ui/core";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
 
 const IndexPage: React.FC<{}> = () => (
   <Layout>
     <SEO title="Index Page" />
     <h1>title</h1>
-    <Image />
     <p>Welcome to your new Gatsby site.</p>
+    <Button variantColor="teal">Click</Button>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
